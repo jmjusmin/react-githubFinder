f23a3fe27c7b18b2602c4f7cbabd06328c4c8e91
jmjusmin <jmjusmin1412@gmail.com>
2022-10-28 17:13:32 -0500

    Add about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
+import { FaGithub } from "react-icons/fa";
+
 function About() {
   return (
-    <div>
-      <h1 className="text-6xl mb-4">Github Finder</h1>
+    <div className="ml-3">
+      <div className="flex content-center">
+        <h1 className="text-6xl mb-4 mr-8">Github Finder</h1>
+        <a
+          href="https://github.com/jmjusmin/react-githubFinder"
+          className="text-3xl text-red-600 hover:text-red-300"
+        >
+          <FaGithub />
+        </a>
+      </div>
+      <p className="mb-3">
+        This is a project in my REACT learning journey where I gain knowledge of
+        REACT hooks and tailwind CSS in practice.
+      </p>
+      <p className="mb-3">
+        GitHub Finder is an application to search GitHub repositories that
+        retrieve a list of GitHub API data and display it accordingly with user
+        search.
+      </p>
     </div>
   );
 }
